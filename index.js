fb3810eabfb18be9202b0844dad8bcd133e202c0
antoniovirgone <antonaccio86@gmail.com>
2025-11-11 23:19:36 +0100

    add db connect

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+const { Client } = require('pg');
+const connectionString = process.env.DATABASE_URL;
+const client = new Client({ connectionString, ssl: { rejectUnauthorized: false }});
+await client.connect();
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
 app.listen(process.env.PORT || 3000, () => {
     console.log('listening on port 3000')
 })
