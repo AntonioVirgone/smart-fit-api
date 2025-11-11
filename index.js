1e69d60b3a17db1ef2e0646012975dca6990595c
antoniovirgone <antonaccio86@gmail.com>
2025-11-11 23:23:17 +0100

    fix type module

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
+import express from 'express';
 
-const { Client } = require('pg');
-const connectionString = process.env.DATABASE_URL;
-const client = new Client({ connectionString, ssl: { rejectUnauthorized: false }});
-await client.connect();
+const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log('listening on port 3000')
-})
+app.get('/', (req, res) => res.send('ok'));
+app.listen(process.env.PORT || 3000, () => console.log('listening'));
