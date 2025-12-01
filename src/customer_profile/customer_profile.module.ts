81608bc8a8f612555c2d9a5c9704c0b5b46e1d7e
antoniovirgone <antonaccio86@gmail.com>
2025-12-01 16:14:29 +0100

    fix

--- src/customer_profile/customer_profile.module.ts
+++ src/customer_profile/customer_profile.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CustomerProfileService } from './customer_profile.service';
 import { CustomerProfileController } from './customer_profile.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomerProfile } from './entities/customer_profile.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CustomerProfile])],
   controllers: [CustomerProfileController],
   providers: [CustomerProfileService],
+  exports: [CustomerProfileService],
 })
 export class CustomerProfileModule {}
